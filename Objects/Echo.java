58bc5a6c316a0a54e0ca94987c41e67543bba9ff
o-pan <o.pan@gatech.edu>
2015-10-02 00:03:09 -0400

    add more files

---
+++ Objects/Echo.java
@@ -0,0 +1,11 @@
+public class Echo {
+
+public int count = 0;
+public void hello() {
+	System.out.println("helloooooo....");
+}
+public String hiThere() {
+	String str = "heygirlhey";
+	return str;
+}
+}
